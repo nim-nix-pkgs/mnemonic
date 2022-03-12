b3f888e5d8e2967b0562a741f27201d3a2e127db
hugosenari <hugosenari@gmail.com>
2022-03-12 03:30:02 -0300

    chore: re index mnemonic

---
+++ v0_1_3/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Create memorable sentences from byte sequences.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mnemonic-v0_1_3.flake = false;
+  inputs.src-mnemonic-v0_1_3.owner = "markspanbroek";
+  inputs.src-mnemonic-v0_1_3.ref   = "refs/tags/v0.1.3";
+  inputs.src-mnemonic-v0_1_3.repo  = "mnemonic";
+  inputs.src-mnemonic-v0_1_3.type  = "github";
+  
+  inputs."nimsha2".dir   = "nimpkgs/n/nimsha2";
+  inputs."nimsha2".owner = "riinr";
+  inputs."nimsha2".ref   = "flake-pinning";
+  inputs."nimsha2".repo  = "flake-nimble";
+  inputs."nimsha2".type  = "github";
+  inputs."nimsha2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimsha2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mnemonic-v0_1_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mnemonic-v0_1_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
